2c1806beea2bb23c789fa61b980e9f4e2762c08b
Gifer-Sasi <sasi.gifer@gmail.com>
2020-03-20 03:35:20 +0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const Config = {
+    apiKey: "AIzaSyAWqo7vK_8mg2kct_mBp4nzywHabjeU4T4",
+    authDomain: "todo-2762e.firebaseapp.com",
+    databaseURL: "https://todo-2762e.firebaseio.com",
+    projectId: "todo-2762e",
+    storageBucket: "todo-2762e.appspot.com",
+    messagingSenderId: "42212830509",
+    appId: "1:42212830509:web:bb26345250bd00a3f5f7a4",
+    measurementId: "G-JXH3XTLEHR"
+  };
+
+  export default Config;
